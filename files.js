a249c948a5f842c55be38586fc7339a6597ed733
wurldwide <142448429+wurldwide@users.noreply.github.com>
2023-08-17 18:30:02 +0100

    Update files.js

--- files.js
+++ files.js
@@ -1,46 +1,42 @@
-<script>const files = document.querySelectorAll('.file')
-let z = 1
-files.forEach((file) => {
-const content = file.querySelector('.content')
-const corner = file.querySelector('.corner')
-file.addEventListener('mousedown', () => {
-z = z + 1
-file.style.zIndex = z
-})
-content.addEventListener('mousedown', (event) => {
-file.classList.add('is-dragging')
-let l = file.offsetLeft
-let t = file.offsetTop
-let startX = event.pageX
-let startY = event.pageY
-const drag = (event) => {
-event.preventDefault()
-file.style.left = l + (event.pageX - startX) + 'px'
-file.style.top = t + (event.pageY - startY) + 'px'
-}
-const mouseup = () => {
-file.classList.remove('is-dragging')
-document.removeEventListener('mousemove', drag)
-document.removeEventListener('mouseup', mouseup)
-}
-document.addEventListener('mousemove', drag)
-document.addEventListener('mouseup', mouseup)
-})
-corner.addEventListener('mousedown', (event) => {
-let w = file.clientWidth
-let h = file.clientHeight
-let startX = event.pageX
-let startY = event.pageY
-const drag = (event) => {
-event.preventDefault()
-file.style.width = w + (event.pageX - startX) + 'px'
-file.style.height = h + (event.pageY - startY) + 'px'
-}
-const mouseup = () => {
-document.removeEventListener('mousemove', drag)
-document.removeEventListener('mouseup', mouseup)
-}
-document.addEventListener('mousemove', drag)
-document.addEventListener('mouseup', mouseup)
-})
-})</script>
+const panes = document.querySelectorAll('.pane');
+
+let z = 1;
+
+panes.forEach((pane) => {
+  const title = pane.querySelector('.title');
+  const content = pane.querySelector('.content'); // Assuming there's a class named 'content' for the pane content
+
+  pane.addEventListener('mousedown', () => {
+    z = z + 1;
+    pane.style.zIndex = z;
+  });
+
+  content.addEventListener('mousedown', (event) => {
+    pane.classList.add('is-dragging');
+
+    let l = pane.offsetLeft;
+    let t = pane.offsetTop;
+
+    let startX = event.pageX;
+    let startY = event.pageY;
+
+    const drag = (event) => {
+      event.preventDefault();
+
+      pane.style.left = l + (event.pageX - startX) + 'px';
+      pane.style.top = t + (event.pageY - startY) + 'px';
+    };
+
+    const mouseup = () => {
+      pane.classList.remove('is-dragging');
+
+      document.removeEventListener('mousemove', drag);
+      document.removeEventListener('mouseup', mouseup);
+    };
+
+    document.addEventListener('mousemove', drag);
+    document.addEventListener('mouseup', mouseup);
+  });
+
+  // ... (Other code remains the same)
+});
